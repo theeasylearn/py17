fff0840b65e399502103f7671a806db763ac4220
theeasylearn <theeasylearn@gmail.com>
2023-10-10 11:20:47 +0530

    first commit

---
+++ lesson1.py
@@ -0,0 +1,3 @@
+print("we have started learning python")
+print("our names are bhautik, vishva, jignasa")
+print("Good bye")
---
+++ lesson2.py
@@ -0,0 +1,8 @@
+year = 2023 #integer 
+print(year)
+
+year = 2023.10 #float
+print(year)
+
+year = "two thousand twenty three"
+print(year)
