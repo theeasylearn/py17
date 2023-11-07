c64e2954aeed7aa8e680813baa7a66cb45a47599
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-11-07 11:29:57 +0530

    full pyramid

---
+++ while_36.py
@@ -0,0 +1,40 @@
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3 
+# 1 2
+# 1
+
+# 5 4 3 2 1
+# 5 4 3 2
+# 5 4 3
+# 5 4 
+# 5 
+
+
+
+i=1
+k=5
+
+while k>=1:
+    while i<=k:
+        print(f"{i} ",end='')
+        i+=1  # i=i+1
+    i=1
+    print("")
+    k-=1
+
+# while i<=4:
+#     print(f"{i} ",end='')
+#     i+=1
+
+# i=1
+# print("")
+
+# while i<=3:
+#     print(f"{i} ",end='')
+#     i+=1
+
+# i=1
+# print("")
+
+
---
+++ while_37.py
@@ -0,0 +1,74 @@
+# _____*
+# ____* *
+# ___* * *
+# __* * * *
+# _* * * * *
+
+# _____*****
+# _____*****
+# _____*****
+# _____*****
+# _____*****
+
+i=1
+j=1
+k=5
+while i<=5:
+    while j<=k:
+        print(" ",end='')
+        j+=1
+
+    j=1
+    while(j<=i):    
+        print("* ",end='')
+        j+=1
+
+    print("")
+    j=1
+    i+=1
+    k-=1
+
+# while j<=4:
+#     print("_",end='')
+#     j+=1
+
+# j=1
+# while(j<=2):    
+#     print("*",end='')
+#     j+=1
+# print("")
+# j=1
+
+# while j<=3:
+#     print("_",end='')
+#     j+=1
+
+# j=1
+# while(j<=3):    
+#     print("*",end='')
+#     j+=1
+
+# print("")
+# j=1
+# while j<=2:
+#     print("_",end='')
+#     j+=1
+
+# j=1
+# while(j<=4):    
+#     print("*",end='')
+#     j+=1
+
+# print("")
+# j=1
+# while j<=1:
+#     print("_",end='')
+#     j+=1
+
+# j=1
+# while(j<=5):    
+#     print("*",end='')
+#     j+=1
+
+# print("")
+# j=1
